8772de0471ecc067638cbee18fb35cbf989d4054
miko <mikozet2@gmail.com>
2022-12-17 11:08:30 +0400

    Change onboarding icons in tutorial process

--- src/components/Finder.js
+++ src/components/Finder.js
@@ -22,7 +22,7 @@ import { generatePath, useHistory } from 'react-router-dom';
 
 import { SEARCH_PATH } from '~/routes';
 
-import TourWebOfTrustSVG from '%/images/tour-web-of-trust.svg';
+import TourWebOfTrustSVG from '%/images/web-of-trust.svg';
 import Button from '~/components/Button';
 import ProfileMini from '~/components/ProfileMini';
 import TabNavigation from '~/components/TabNavigation';
